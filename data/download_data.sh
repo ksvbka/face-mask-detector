92f070633caaef2f4809fe95a1b7199fa463c823
Trung Kien <letrungkien.k53.hut@gmail.com>
2020-12-12 21:43:42 +0700

    Add more test data, update download link

--- data/download_data.sh
+++ data/download_data.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # NOTE: If it failed to auto get file, please visit link to manual download
-#       https://drive.google.com/u/0/uc?id=1SvfU09VRDjtvKAF2u0ylDFfjLRensc77&export=download
+#       https://drive.google.com/u/0/uc?id=1PCvvxm4SCalT2iJon47hbu0rn6wjbuj5&export=download
 
-wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1SvfU09VRDjtvKAF2u0ylDFfjLRensc77' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1SvfU09VRDjtvKAF2u0ylDFfjLRensc77" -O dataset_raw.zip && rm -rf /tmp/cookies.txt
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1PCvvxm4SCalT2iJon47hbu0rn6wjbuj5' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1PCvvxm4SCalT2iJon47hbu0rn6wjbuj5" -O dataset_raw.zip && rm -rf /tmp/cookies.txt
 unzip dataset_raw.zip
